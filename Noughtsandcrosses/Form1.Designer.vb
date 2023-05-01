a8ad3f15a23f985d416dca62dfb12eeaad84dfd8
NapoliGit <132287943+NapoliGit@users.noreply.github.com>
2023-05-01 17:30:45 +0100

    Add files via upload

---
+++ Noughtsandcrosses/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TL = New System.Windows.Forms.Button()
+        Me.TM = New System.Windows.Forms.Button()
+        Me.TR = New System.Windows.Forms.Button()
+        Me.ML = New System.Windows.Forms.Button()
+        Me.MM = New System.Windows.Forms.Button()
+        Me.MR = New System.Windows.Forms.Button()
+        Me.BL = New System.Windows.Forms.Button()
+        Me.BM = New System.Windows.Forms.Button()
+        Me.BR = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TL
+        '
+        Me.TL.Location = New System.Drawing.Point(279, 76)
+        Me.TL.Name = "TL"
+        Me.TL.Size = New System.Drawing.Size(70, 66)
+        Me.TL.TabIndex = 0
+        Me.TL.UseVisualStyleBackColor = True
+        '
+        'TM
+        '
+        Me.TM.Location = New System.Drawing.Point(355, 76)
+        Me.TM.Name = "TM"
+        Me.TM.Size = New System.Drawing.Size(70, 66)
+        Me.TM.TabIndex = 1
+        Me.TM.UseVisualStyleBackColor = True
+        '
+        'TR
+        '
+        Me.TR.Location = New System.Drawing.Point(431, 76)
+        Me.TR.Name = "TR"
+        Me.TR.Size = New System.Drawing.Size(70, 66)
+        Me.TR.TabIndex = 2
+        Me.TR.UseVisualStyleBackColor = True
+        '
+        'ML
+        '
+        Me.ML.Location = New System.Drawing.Point(279, 148)
+        Me.ML.Name = "ML"
+        Me.ML.Size = New System.Drawing.Size(70, 66)
+        Me.ML.TabIndex = 5
+        Me.ML.UseVisualStyleBackColor = True
+        '
+        'MM
+        '
+        Me.MM.Location = New System.Drawing.Point(355, 148)
+        Me.MM.Name = "MM"
+        Me.MM.Size = New System.Drawing.Size(70, 66)
+        Me.MM.TabIndex = 4
+        Me.MM.UseVisualStyleBackColor = True
+        '
+        'MR
+        '
+        Me.MR.Location = New System.Drawing.Point(431, 148)
+        Me.MR.Name = "MR"
+        Me.MR.Size = New System.Drawing.Size(70, 66)
+        Me.MR.TabIndex = 3
+        Me.MR.UseVisualStyleBackColor = True
+        '
+        'BL
+        '
+        Me.BL.Location = New System.Drawing.Point(279, 220)
+        Me.BL.Name = "BL"
+        Me.BL.Size = New System.Drawing.Size(70, 66)
+        Me.BL.TabIndex = 8
+        Me.BL.UseVisualStyleBackColor = True
+        '
+        'BM
+        '
+        Me.BM.Location = New System.Drawing.Point(355, 220)
+        Me.BM.Name = "BM"
+        Me.BM.Size = New System.Drawing.Size(70, 66)
+        Me.BM.TabIndex = 7
+        Me.BM.UseVisualStyleBackColor = True
+        '
+        'BR
+        '
+        Me.BR.Location = New System.Drawing.Point(431, 220)
+        Me.BR.Name = "BR"
+        Me.BR.Size = New System.Drawing.Size(70, 66)
+        Me.BR.TabIndex = 6
+        Me.BR.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label1.Location = New System.Drawing.Point(231, 310)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(318, 31)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "WINNER : To be decided"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Label2.Location = New System.Drawing.Point(134, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(551, 39)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Tic Tac Toe / Noughts and Crosses"
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(610, 191)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 11
+        Me.Button10.Text = "RESTART"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BL)
+        Me.Controls.Add(Me.BM)
+        Me.Controls.Add(Me.BR)
+        Me.Controls.Add(Me.ML)
+        Me.Controls.Add(Me.MM)
+        Me.Controls.Add(Me.MR)
+        Me.Controls.Add(Me.TR)
+        Me.Controls.Add(Me.TM)
+        Me.Controls.Add(Me.TL)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TL As Button
+    Friend WithEvents TM As Button
+    Friend WithEvents TR As Button
+    Friend WithEvents ML As Button
+    Friend WithEvents MM As Button
+    Friend WithEvents MR As Button
+    Friend WithEvents BL As Button
+    Friend WithEvents BM As Button
+    Friend WithEvents BR As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button10 As Button
+End Class
---
+++ Noughtsandcrosses/Form1.vb
@@ -0,0 +1,232 @@
+﻿Public Class Form1
+    Dim user As Char() = New Char() {"X", "O"}
+    Dim xturn As Boolean = False
+    Dim result As String
+    Dim check As String
+
+    Public Sub checkbuttons()
+        If (TL.Text = "O" Or TL.Text = "X") And (TM.Text = "O" Or TM.Text = "X") And (TR.Text = "O" Or TR.Text = "X") And (ML.Text = "O" Or ML.Text = "X") And (MM.Text = "O" Or MM.Text = "X") And (MR.Text = "O" Or MR.Text = "X") And (BL.Text = "O" Or BL.Text = "X") And (BM.Text = "O" Or BM.Text = "X") And (BR.Text = "O" Or BR.Text = "X") Then
+            Label1.Text = "WINNER : DRAW"
+            TL.Enabled = False
+            TM.Enabled = False
+            TR.Enabled = False
+            ML.Enabled = False
+            MM.Enabled = False
+            MR.Enabled = False
+            BL.Enabled = False
+            BM.Enabled = False
+            BR.Enabled = False
+
+
+        End If
+    End Sub
+    Public Sub win()
+
+        ' horizontal combinations :
+        If TL.Text = TM.Text And TM.Text = TR.Text Then
+            Label1.Text = "WINNER : " + TM.Text
+
+        ElseIf ML.Text = MM.Text And MM.Text = MR.Text Then
+            Label1.Text = "WINNER : " + MM.Text
+
+        ElseIf BL.Text = BM.Text And BM.Text = BR.Text Then
+            Label1.Text = "WINNER : " + BM.Text
+
+            ' vertical combinations :
+
+        ElseIf TL.Text = ML.Text And ML.Text = BL.Text Then
+            Label1.Text = "WINNER : " + ML.Text
+        ElseIf TM.Text = MM.Text And MM.Text = BM.Text Then
+            Label1.Text = "WINNER : " + MM.Text
+        ElseIf TR.Text = MR.Text And MR.Text = BR.Text Then
+            Label1.Text = "WINNER : " + MR.Text
+
+            ' diagonal combinations :
+
+        ElseIf TL.Text = MM.Text And MM.Text = BR.Text Then
+            Label1.Text = "WINNER : " + MM.Text
+        ElseIf TR.Text = MM.Text And MM.Text = BL.Text Then
+            Label1.Text = "WINNER : " + MM.Text
+        Else Call checkbuttons()
+
+
+
+        End If
+
+        If Label1.Text = "WINNER : X" Or Label1.Text = "WINNER : O" Then
+
+            TL.Enabled = False
+            TM.Enabled = False
+            TR.Enabled = False
+            ML.Enabled = False
+            MM.Enabled = False
+            MR.Enabled = False
+            BL.Enabled = False
+            BM.Enabled = False
+            BR.Enabled = False
+
+        End If
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles TL.Click
+        If xturn = False Then
+            TL.Text = user(0)
+
+            xturn = True
+            TL.Enabled = False
+        Else
+            TL.Text = user(1)
+            xturn = False
+            TL.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles TM.Click
+        If xturn = False Then
+            TM.Text = user(0)
+            xturn = True
+            TM.Enabled = False
+
+        Else
+            TM.Text = user(1)
+            xturn = False
+            TM.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles TR.Click
+        If xturn = False Then
+            TR.Text = user(0)
+            xturn = True
+            TR.Enabled = False
+
+        Else
+            TR.Text = user(1)
+            xturn = False
+            TR.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles ML.Click
+        If xturn = False Then
+            ML.Text = user(0)
+            xturn = True
+            ML.Enabled = False
+
+        Else
+            ML.Text = user(1)
+            xturn = False
+            ML.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles MM.Click
+        If xturn = False Then
+            MM.Text = user(0)
+            xturn = True
+            MM.Enabled = False
+
+        Else
+            MM.Text = user(1)
+            xturn = False
+            MM.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles MR.Click
+        If xturn = False Then
+            MR.Text = user(0)
+            xturn = True
+            MR.Enabled = False
+
+        Else
+            MR.Text = user(1)
+            xturn = False
+            MR.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles BL.Click
+        If xturn = False Then
+            BL.Text = user(0)
+            xturn = True
+            BL.Enabled = False
+
+        Else
+            BL.Text = user(1)
+            xturn = False
+            BL.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles BM.Click
+        If xturn = False Then
+            BM.Text = user(0)
+            xturn = True
+            BM.Enabled = False
+
+        Else
+            BM.Text = user(1)
+            xturn = False
+            BM.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles BR.Click
+        If xturn = False Then
+            BR.Text = user(0)
+            xturn = True
+            BR.Enabled = False
+
+        Else
+            BR.Text = user(1)
+            xturn = False
+            BR.Enabled = False
+
+        End If
+        Call win()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TL.Enabled = True
+        TM.Enabled = True
+        TR.Enabled = True
+        ML.Enabled = True
+        MM.Enabled = True
+        MR.Enabled = True
+        BL.Enabled = True
+        BM.Enabled = True
+        BR.Enabled = True
+        TL.Text = ""
+        TM.Text = ""
+        TR.Text = ""
+        ML.Text = ""
+        MM.Text = ""
+        MR.Text = ""
+        BL.Text = ""
+        BM.Text = ""
+        BR.Text = ""
+    End Sub
+End Class
